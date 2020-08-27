7181e2f5102bb73717ff8f7eddcef99e45508f9d
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-08-27 19:00:20 -0300

    styling tooltip by inheritance

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -4,11 +4,12 @@ import { Container } from './styles';
 
 interface TooltipProps {
   title: string;
+  className?: string;
 }
 
-const Tooltip: React.FC<TooltipProps> = ({ title, children }) => {
+const Tooltip: React.FC<TooltipProps> = ({ title, className, children }) => {
   return (
-    <Container>
+    <Container className={className}>
       {children}
       <span>{title}</span>
     </Container>
