46944a415e6bb67f0e068a5d7d8a86660bbaa183
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-08-26 23:51:27 -0300

    validating user registration with yup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
@@ -9,9 +10,23 @@ import Button from '../../components/Button/index';
 import Input from '../../components/Input/index';
 
 const SignIn: React.FC = () => {
-  const handleSubmit = (data: object): void => {
-    console.log(data);
-  };
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
---
+++ src/utils/validationErrors.ts
@@ -0,0 +1,5 @@
+import { ValidationError } from 'yup';
+
+export default function getValidationErrors(err: ValidationError) {
+  const errors = [];
+}
