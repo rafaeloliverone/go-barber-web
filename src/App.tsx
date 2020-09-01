7560b725cf4ee20bcc8cd2b9f40f8f4908832681
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-09-01 16:46:11 -0300

    api of context

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn/index';
 // import SignUp from './pages/SignUp/index';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Rafael' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
